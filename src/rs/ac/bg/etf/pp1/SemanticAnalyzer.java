23fb3421fca4923e4686cd641cb60b875e2e2ce5
Fedja Vujicic <64490772+FedjaVujicic@users.noreply.github.com>
2024-01-08 09:03:10 +0100

    Change report_info and report_error messages

--- src/rs/ac/bg/etf/pp1/SemanticAnalyzer.java
+++ src/rs/ac/bg/etf/pp1/SemanticAnalyzer.java
@@ -1,4 +1,5 @@
 package rs.ac.bg.etf.pp1;
+
 import org.apache.log4j.Logger;
 
 import rs.ac.bg.etf.pp1.ast.*;
@@ -9,38 +10,36 @@ import rs.etf.pp1.symboltable.concepts.Struct;
 public class SemanticAnalyzer extends VisitorAdaptor {
 
 	boolean errorDetected = false;
-	
+
 	Logger log = Logger.getLogger(getClass());
 
 	public void report_error(String message, SyntaxNode info) {
 		errorDetected = true;
 		StringBuilder msg = new StringBuilder(message);
-		int line = (info == null) ? 0: info.getLine();
+		int line = (info == null) ? 0 : info.getLine();
 		if (line != 0)
-			msg.append (" na liniji ").append(line);
+			msg.append(" (line ").append(line).append(")");
 		log.error(msg.toString());
 	}
 
 	public void report_info(String message, SyntaxNode info) {
-		StringBuilder msg = new StringBuilder(message); 
-		int line = (info == null) ? 0: info.getLine();
+		StringBuilder msg = new StringBuilder(message);
+		int line = (info == null) ? 0 : info.getLine();
 		if (line != 0)
-			msg.append (" na liniji ").append(line);
+			msg.append(" (line ").append(line).append(")");
 		log.info(msg.toString());
 	}
-	
+
 	// Program
 	public void visit(Program program) {
 		Obj progObj = program.getProgName().obj;
 		Tab.chainLocalSymbols(progObj);
 		Tab.closeScope();
 	}
-	
+
 	public void visit(ProgName progName) {
 		progName.obj = Tab.insert(Obj.Prog, progName.getProgName(), Tab.noType);
 		Tab.openScope();
 	}
-		
-	
-}
 
+}
