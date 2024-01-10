8a22c496f8cfd3ceb6ef1e191c971a3d16a61f30
Fedja Vujicic <64490772+FedjaVujicic@users.noreply.github.com>
2024-01-10 12:26:38 +0100

    Reformat Compiler.java

--- test/rs/ac/bg/etf/pp1/Compiler.java
+++ test/rs/ac/bg/etf/pp1/Compiler.java
@@ -20,52 +20,50 @@ public class Compiler {
 		DOMConfigurator.configure(Log4JUtils.instance().findLoggerConfigFile());
 		Log4JUtils.instance().prepareLogFile(Logger.getRootLogger());
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		Logger log = Logger.getLogger(Compiler.class);
 		if (args.length < 1) {
 			log.error("Not enough arguments supplied! Usage: MJParser <source-file> <obj-file> ");
 			return;
 		}
-		
+
 		File sourceCode = new File(args[0]);
 		if (!sourceCode.exists()) {
 			log.error("Source file [" + sourceCode.getAbsolutePath() + "] not found!");
 			return;
 		}
-			
+
 		log.info("Compiling source file: " + sourceCode.getAbsolutePath());
-		
+
 		try (BufferedReader br = new BufferedReader(new FileReader(sourceCode))) {
 			Yylex lexer = new Yylex(br);
 			MJParser p = new MJParser(lexer);
-	        Symbol s = p.parse();  //pocetak parsiranja
-	        SyntaxNode prog = (SyntaxNode)(s.value);
-	        
+			Symbol s = p.parse(); // pocetak parsiranja
+			SyntaxNode prog = (SyntaxNode) (s.value);
+
 			if (p.errorDetected) {
 				log.error("Parsing failed!");
 				return;
 			}
 			log.info("Parsing successful!");
-			
+
 			System.out.println("=======================SEMANTIC ANALYSIS=======================");
-	        
+
 			Tab.init(); // Universe scope
 			SemanticAnalyzer semanticCheck = new SemanticAnalyzer();
 			prog.traverseBottomUp(semanticCheck);
-			
-	        Tab.dump();
+
+			Tab.dump();
 			System.out.println("===============================================================");
-			
+
 			if (!semanticCheck.passed()) {
 				log.error("Semantic check failed!");
 				return;
 			}
 			log.info("Semantic check passed!");
-			
-	        log.info("Compiling finished!");
+
+			log.info("Compiling finished!");
 		}
 	}
 }
-
-
