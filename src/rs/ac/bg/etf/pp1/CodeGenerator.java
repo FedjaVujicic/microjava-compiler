3a01e226e77c40136c4579c6623dbc29e673451c
Fedja Vujicic <64490772+FedjaVujicic@users.noreply.github.com>
2024-01-12 09:27:11 +0100

    Add codegen for numeric expressions

--- src/rs/ac/bg/etf/pp1/CodeGenerator.java
+++ src/rs/ac/bg/etf/pp1/CodeGenerator.java
@@ -1,5 +1,7 @@
 package rs.ac.bg.etf.pp1;
 
+import java.util.Stack;
+
 import rs.ac.bg.etf.pp1.CounterVisitor.FormParCounter;
 import rs.ac.bg.etf.pp1.CounterVisitor.VarCounter;
 import rs.ac.bg.etf.pp1.ast.*;
@@ -9,6 +11,17 @@ import rs.etf.pp1.symboltable.concepts.*;
 public class CodeGenerator extends VisitorAdaptor {
 
 	private int mainPc;
+
+	Stack<MulOper> nextMulOp = new Stack<MulOper>();
+	Stack<AddOper> nextAddOp = new Stack<AddOper>();
+
+	enum MulOper {
+		MUL, DIV, REM
+	};
+
+	enum AddOper {
+		ADD, SUB
+	};
 
 	public int getMainPc() {
 		return mainPc;
@@ -45,7 +58,55 @@ public class CodeGenerator extends VisitorAdaptor {
 		Code.put(Code.print);
 	}
 
+	public void visit(ExprAddTerm exprAddTerm) {
+		AddOper op = nextAddOp.pop();
+		switch (op) {
+		case ADD:
+			Code.put(Code.add);
+			break;
+		case SUB:
+			Code.put(Code.sub);
+			break;
+		}
+	}
+
+	public void visit(OpAdd opAdd) {
+		nextAddOp.push(AddOper.ADD);
+	}
+
+	public void visit(OpSub opSub) {
+		nextAddOp.push(AddOper.SUB);
+	}
+
+	public void visit(TermMulFactor termMulFactor) {
+		MulOper op = nextMulOp.pop();
+		switch (op) {
+		case MUL:
+			Code.put(Code.mul);
+			break;
+		case DIV:
+			Code.put(Code.div);
+			break;
+		case REM:
+			Code.put(Code.rem);
+			break;
+		}
+	}
+
+	public void visit(OpMul opMul) {
+		nextMulOp.push(MulOper.MUL);
+	}
+
+	public void visit(OpDiv opDiv) {
+		nextMulOp.push(MulOper.DIV);
+	}
+
+	public void visit(OpMod opMod) {
+		nextMulOp.push(MulOper.REM);
+	}
+
 	public void visit(FactorNum factorNum) {
 		Code.load(new Obj(Obj.Con, "$", factorNum.struct, factorNum.getNumVal(), 0));
 	}
+
 }
